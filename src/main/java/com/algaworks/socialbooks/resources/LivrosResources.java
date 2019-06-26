ccbf534b07dae96d3f913e5e6e4ff4f2d5f04670
lucas.ramos <lucas.ramos@sonda.com>
2019-06-26 13:18:44 -0300

    Desenvolvendo delete, search e update

--- src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
+++ src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
@@ -3,6 +3,7 @@ package com.algaworks.socialbooks.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,5 +28,22 @@ public class LivrosResources {
 	public void salvar(@RequestBody Livro livro) {
 		livrosRepository.save(livro);
 	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public Livro buscar(@PathVariable("id") Long id) {
+		return livrosRepository.findById(id).get();
+	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.DELETE)
+	public void deletar(@PathVariable("id") Long id) {
+		livrosRepository.deleteById(id);
+	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.PUT)
+	public void atualizar(@RequestBody Livro livro, @PathVariable("id") Long id) {
+		livro.setId(id);
+		livrosRepository.save(livro);
+	}
+	
 	
 }
