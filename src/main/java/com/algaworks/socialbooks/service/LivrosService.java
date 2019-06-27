14e8f080874e8f0438bca6d1108f42d19786762a
lucas.ramos <lucas.ramos@sonda.com>
2019-06-27 16:34:21 -0300

    Listando comentários

--- src/main/java/com/algaworks/socialbooks/service/LivrosService.java
+++ src/main/java/com/algaworks/socialbooks/service/LivrosService.java
@@ -71,4 +71,10 @@ public class LivrosService {
 		
 		return comentariosRepository.save(comentario);
 	}
+	
+	public List<Comentario> listarComentarios(Long livroId) {
+		Livro livro = buscar(livroId);
+		
+		return livro.getComentarios();
+	}
 }
