6eaad780f7e3c90b1845b8ee21d4708d9aa01ec0
lucas.ramos <lucas.ramos@sonda.com>
2019-07-02 13:25:48 -0300

    evoluindo o recurso autor

--- src/main/java/com/algaworks/socialbooks/handler/ResourceExceptionHandler.java
+++ src/main/java/com/algaworks/socialbooks/handler/ResourceExceptionHandler.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.algaworks.socialbooks.domain.DetalhesErro;
+import com.algaworks.socialbooks.service.exceptions.AutorExistenteException;
+import com.algaworks.socialbooks.service.exceptions.AutorNaoEncontradoException;
 import com.algaworks.socialbooks.service.exceptions.LivroNaoEncontradoException;
 
 @ControllerAdvice
@@ -20,6 +22,32 @@ public class ResourceExceptionHandler {
 		DetalhesErro erro = new DetalhesErro();
 		erro.setStatus(4041);
 		erro.setTitulo("O livro não pôde ser encontrado");
+		erro.setMensagemDesenvolvedor("http://erros.socialbooks.com/404");
+		erro.setTimestamp(System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+	}
+	
+	@ExceptionHandler(AutorExistenteException.class)
+	public ResponseEntity<DetalhesErro> handlerAutorJaExistente(AutorExistenteException e,
+			HttpServletRequest request) {
+		
+		DetalhesErro erro = new DetalhesErro();
+		erro.setStatus(409);
+		erro.setTitulo("O autor já existente");
+		erro.setMensagemDesenvolvedor("http://erros.socialbooks.com/409");
+		erro.setTimestamp(System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.CONFLICT).body(erro);
+	}
+	
+	@ExceptionHandler(AutorNaoEncontradoException.class)
+	public ResponseEntity<DetalhesErro> handlerAutorNaoEncontrado(AutorNaoEncontradoException e,
+			HttpServletRequest request) {
+		
+		DetalhesErro erro = new DetalhesErro();
+		erro.setStatus(404);
+		erro.setTitulo("O autor não encontrado");
 		erro.setMensagemDesenvolvedor("http://erros.socialbooks.com/404");
 		erro.setTimestamp(System.currentTimeMillis());
 		
--- src/main/java/com/algaworks/socialbooks/service/AutoresService.java
+++ src/main/java/com/algaworks/socialbooks/service/AutoresService.java
@@ -1,12 +1,15 @@
 package com.algaworks.socialbooks.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.algaworks.socialbooks.domain.Autor;
 import com.algaworks.socialbooks.repository.AutoresRepository;
+import com.algaworks.socialbooks.service.exceptions.AutorExistenteException;
+import com.algaworks.socialbooks.service.exceptions.AutorNaoEncontradoException;
 
 @Service
 public class AutoresService {
@@ -16,5 +19,29 @@ public class AutoresService {
 	
 	public List<Autor> listar(){
 		return autoresRepository.findAll();
+	}
+	
+	public Autor salvar(Autor autor) {
+		
+		if(autor.getId() != null) {
+			Optional<Autor> retornoAutor = autoresRepository.findById(autor.getId());
+			
+			if(retornoAutor.isPresent()) {
+				throw new AutorExistenteException("O autor já existe!");
+			}			
+		}
+		
+		return autoresRepository.save(autor);
+	}
+	
+	public Autor buscar(Long id) {
+		
+		Optional<Autor> autorEncontrado = autoresRepository.findById(id);
+		
+		if(!autorEncontrado.isPresent()) {
+			throw new AutorNaoEncontradoException("O autor não pôde ser encontrado!");
+		}
+		
+		return autorEncontrado.get();
 	}
 }
